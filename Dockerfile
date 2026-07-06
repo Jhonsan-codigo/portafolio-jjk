a4f5b9ab8327d54f9474826707f28c4a0c3fdf59
Jhonsan-codigo <zuasnabarhinostroza@gmail.com>
2026-07-06 17:30:11 -0500

    Fix: Agregar driver PostgreSQL en Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,10 @@ FROM tomcat:10.1-jdk17
 # Instalar Maven
 RUN apt-get update && apt-get install -y maven
 
+# Descargar driver PostgreSQL directamente
+RUN apt-get install -y wget && \
+    wget https://jdbc.postgresql.org/download/postgresql-42.7.1.jar -O /usr/local/tomcat/lib/postgresql-42.7.1.jar
+
 # Copiar todo el código fuente
 COPY . /app
 WORKDIR /app
--- src/main/java/com/zonajava/servlets/ConexionDB.java
+++ src/main/java/com/zonajava/servlets/ConexionDB.java
@@ -8,10 +8,11 @@ public class ConexionDB {
         String renderDb = System.getenv("DATABASE_URL");
         if (renderDb != null) {
             Class.forName("org.postgresql.Driver");
-            String url = renderDb.replace("postgres://", "jdbc:postgresql://");
+            // Forzar jdbc:postgresql://
+            String url = "jdbc:postgresql://dpg-d960o89kh4rs73aui54g-a.oregon-postgres.render.com:5432/portafolio_jjk_db?user=portafolio_jjk_db_user&password=46KYo6TcoE4KWkGWr5mxaCIh7sra8rl8&sslmode=require";
             return DriverManager.getConnection(url);
         } else {
-           Connection conn = ConexionDB.getConnection();
+            Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/portafolio_jjk", "root", "");
         }
